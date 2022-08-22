ab6edf5e9991aa6609fbf05df68d1e7e4307ac8b
Gideon Idoko <iamgideonidoko@gmail.com>
2022-08-22 12:46:01 +0100

    Update urls

--- helper.ts
+++ helper.ts
@@ -5,9 +5,13 @@ config();
 type Constant = {
     fileBaseUrl: string;
     dirBaseUrl: string;
+    fileBaseUrl2: string;
+    dirBaseUrl2: string;
 };
 
 export const constants: Constant = {
     fileBaseUrl: 'https://raw.githubusercontent.com/Ebazhanov/linkedin-skill-assessments-quizzes/master',
     dirBaseUrl: 'https://api.github.com/repos/Ebazhanov/linkedin-skill-assessments-quizzes',
+    fileBaseUrl2: 'https://raw.githubusercontent.com/IamGideonIdoko/linkedin-skill-assessments-quizzes/master',
+    dirBaseUrl2: 'https://api.github.com/repos/IamGideonIdoko/linkedin-skill-assessments-quizzes',
 };
